232ffbf013cd909df0042823b0b06f25783d2a95
Leonel Caroc <leonelcaroc25@gmail.com>
2023-09-06 15:36:23 +0800

    update files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,9 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routers/Routers.jsx";
 import "./index.css";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
-);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
