5e73a6f4ced628ed18cf29e2380f134adc092fe6
leonelcaroc <leonelcaroc25@gmail.com>
2025-10-14 17:58:47 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -32,16 +32,28 @@ app.use(
 );
 app.disable("x-powered-by");
 
-const corsOptions = {
-  origin: ["http://localhost:3000"],
-  credentials: true,
-};
+// const corsOptions = {
+//   // origin: ["http://localhost:3000"],
+//   origin: ["http://chessportal.org"],
+//   credentials: true,
+// };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(cookieParser());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  const allowedIPs = ["::1", "127.0.0.1"]; // IPv6 and IPv4 localhost
+  const clientIP = req.ip || req.socket.remoteAddress;
+
+  if (!allowedIPs.includes(clientIP)) {
+    return res.status(403).json({ message: "Access denied" });
+  }
+
+  next();
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/search", searchRoutes);
